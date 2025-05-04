178415e68d594b36b6d648e1261162b03cb4c28b
dyingpotato890 <niranjayajayan2004@gmail.com>
2025-05-05 01:35:48 +0530

    fix display bug

--- lib/pages/operation_page.dart
+++ lib/pages/operation_page.dart
@@ -56,116 +56,111 @@ class _OperationPageState extends State<OperationPage> {
 
                   SizedBox(height: media.height * 0.06),
 
-                  SizedBox(
-                    height: media.height * 0.53,
-                    child: Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.all(24),
-                        decoration: BoxDecoration(
-                          color: Colors.white.withAlpha(51),
-                          borderRadius: BorderRadius.circular(24),
-                        ),
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(24),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withAlpha(51),
+                        borderRadius: BorderRadius.circular(24),
+                      ),
 
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  // Following
-                                  FileSelector(
-                                    icon: Icons.people_outline,
-                                    label: 'Select Following List',
-                                    filename: 'following.json',
-                                    onTap: () async {
-                                      FilePickerResult? result =
-                                          await FilePicker.platform.pickFiles();
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                // Following
+                                FileSelector(
+                                  icon: Icons.people_outline,
+                                  label: 'Select Following List',
+                                  filename: 'following.json',
+                                  onTap: () async {
+                                    FilePickerResult? result =
+                                        await FilePicker.platform.pickFiles();
 
-                                      if (result != null) {
-                                        followingPath =
-                                            result.files.single.path!;
-                                      }
+                                    if (result != null) {
+                                      followingPath = result.files.single.path!;
+                                    }
+                                  },
+                                ),
+
+                                SizedBox(height: media.height * 0.05),
+
+                                // Followers
+                                FileSelector(
+                                  icon: Icons.person_add_outlined,
+                                  label: 'Select Followers List',
+                                  filename: 'followers_1.json',
+                                  onTap: () async {
+                                    FilePickerResult? result =
+                                        await FilePicker.platform.pickFiles();
+
+                                    if (result != null) {
+                                      followersPath = result.files.single.path!;
+                                    }
+                                  },
+                                ),
+
+                                SizedBox(height: media.height * 0.09),
+
+                                // Submit Button
+                                SizedBox(
+                                  height: media.height * 0.046,
+                                  width: media.width * 0.78,
+                                  child: ElevatedButton(
+                                    onPressed: () async {
+                                      remove = await obj.findUnfollowed(
+                                        followersPath: followersPath,
+                                        followingPath: followingPath,
+                                      );
+
+                                      Navigator.push(
+                                        // ignore: use_build_context_synchronously
+                                        context,
+                                        MaterialPageRoute(
+                                          builder:
+                                              (context) => DisplayPage(
+                                                unfollowers: remove,
+                                              ),
+                                        ),
+                                      );
                                     },
-                                  ),
-
-                                  const SizedBox(height: 40),
-
-                                  // Followers
-                                  FileSelector(
-                                    icon: Icons.person_add_outlined,
-                                    label: 'Select Followers List',
-                                    filename: 'followers_1.json',
-                                    onTap: () async {
-                                      FilePickerResult? result =
-                                          await FilePicker.platform.pickFiles();
-
-                                      if (result != null) {
-                                        followersPath =
-                                            result.files.single.path!;
-                                      }
-                                    },
-                                  ),
-
-                                  const SizedBox(height: 80),
-
-                                  // Submit Button
-                                  SizedBox(
-                                    height: media.height * 0.046,
-                                    width: media.width * 0.78,
-                                    child: ElevatedButton(
-                                      onPressed: () async {
-                                        remove = await obj.findUnfollowed(
-                                          followersPath: followersPath,
-                                          followingPath: followingPath,
-                                        );
-
-                                        Navigator.push(
-                                          // ignore: use_build_context_synchronously
-                                          context,
-                                          MaterialPageRoute(
-                                            builder:
-                                                (context) => DisplayPage(
-                                                  unfollowers: remove,
-                                                ),
-                                          ),
-                                        );
-                                      },
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor: Colors.white,
-                                        foregroundColor: const Color.fromRGBO(
-                                          88,
-                                          81,
-                                          219,
-                                          1,
-                                        ),
-                                        // padding: const EdgeInsets.symmetric(vertical: 16),
-                                        shadowColor: Color.fromRGBO(
-                                          88,
-                                          81,
-                                          219,
-                                          1,
-                                        ),
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(12),
-                                        ),
-                                        elevation: 0,
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.white,
+                                      foregroundColor: const Color.fromRGBO(
+                                        88,
+                                        81,
+                                        219,
+                                        1,
                                       ),
-                                      child: const Text(
-                                        'FIND UNFOLLOWERS',
-                                        style: TextStyle(
-                                          fontFamily: 'Ubuntu',
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                        ),
+                                      // padding: const EdgeInsets.symmetric(vertical: 16),
+                                      shadowColor: Color.fromRGBO(
+                                        88,
+                                        81,
+                                        219,
+                                        1,
+                                      ),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(12),
+                                      ),
+                                      elevation: 0,
+                                    ),
+                                    child: const Text(
+                                      'FIND UNFOLLOWERS',
+                                      style: TextStyle(
+                                        fontFamily: 'Ubuntu',
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
                                       ),
                                     ),
                                   ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
